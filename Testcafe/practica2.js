e9a114fdb31b06b427417a4856edd0ffc9cb618c
Ulises Zenteno <claveforanea@gmail.com>
2020-10-15 21:35:55 -0500

    Primeras practicas

---
+++ Testcafe/practica2.js
@@ -0,0 +1,9 @@
+import {Selector} from 'testcafe';
+import page from './pageModel';
+
+fixture('Practica 2 con testcafe')
+    .page('https://the-internet.herokuapp.com/');
+
+test('Probar dar click en un enlace', async t => { 
+
+});
