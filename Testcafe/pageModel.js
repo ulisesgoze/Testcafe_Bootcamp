951f05cb4ae47414592246e096057b98de26b549
Ulises Zenteno <claveforanea@gmail.com>
2020-10-20 23:56:01 -0500

    agregar practicas 5 y 6

--- Testcafe/pageModel.js
+++ Testcafe/pageModel.js
@@ -14,14 +14,24 @@ class Page {
     this.text33 = Selector('#content');
 
     //Practica 4
-    this.link4 = Selector('a').withText('Checkboxes');     
+    this.link4 = Selector('a').withText('Checkboxes');
     
-    //Checkbox Padre    
-    this.baseCheckbox = Selector('#checkboxes > input[type=checkbox]')      
+    //Checkbox Padre
+    this.baseCheckbox = Selector('#checkboxes > input[type=checkbox]')
     
-    //Checboxs Hijos  
+    //Checboxs Hijos
     //this.checkbox41 = Selector('#checkboxes > input[type=checkbox]:nth-child(1)');    
     this.checkbox42 = Selector('#checkboxes > input[type=checkbox]:nth-child(3)');
+
+    //Practica5
+    this.link5 = Selector('a').withText('Inputs');
+    this.input51 = Selector('#content > div > div > div > input[type=number]');
+
+    //Practica6
+    this.link6 = Selector('a').withText('Add/Remove Elements');
+    this.Btn61 = Selector('#content > div > button');
+    this.removeBtn62 = Selector('#content');
+
     }
 }
-export default new Page();
+export default new Page()
